ce95850183c617a47c02d9bab918f2aa22568cc8
antyadev <antyadev@gmail.com>
2013-03-06 22:35:38 +0200

    Added an initial ExceptionHandlingAspect

---
+++ examples/VS 2010/KingAOP.Examples/ExceptionHandling/ExceptionHandlingAspect.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using KingAOP.Aspects;
+
+namespace KingAOP.Examples.ExceptionHandling
+{
+    class ExceptionHandlingAspect : OnMethodBoundaryAspect
+    {
+        public override void OnException(MethodExecutionArgs args)
+        {
+            if (args.Instance != null)
+            {
+                var instType = args.Instance.GetType();
+                foreach (var property in instType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+                {
+                    
+                }
+            }
+        }
+    }
+}
