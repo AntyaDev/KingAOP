92e03622c19615e5df38d255463bd6df06855c78
Antya Dev <antyadev@gmail.com>
2013-06-04 18:01:01 +0300

    Added ArgumentValidationAspect for examples project.
    I have found bug with ref int (System.Int32 ByRef) type for method
    argument.
    For more info look on it
    http://stackoverflow.com/questions/2475310/c-sharp-4-0-dynamic-doesnt-set-ref-out-arguments

---
+++ examples/VS 2010/KingAOP.Examples/InterceptInvocation/ArgumentValidationAspect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KingAOP.Aspects;
+
+namespace KingAOP.Examples.InterceptInvocation
+{
+    class ArgumentValidationAspect : MethodInterceptionAspect
+    {
+        public override void OnInvoke(MethodInterceptionArgs args)
+        {
+            if ((int)args.Arguments[0] < 0) Console.WriteLine("The first argument is less then 0 and we will not invoke " + args.Method.Name);
+
+            else args.Proceed();
+        }
+    }
+}
