f063ced8e5c5222156699ac3f562fb1d7281fb10
Antya Dev <antyadev@gmail.com>
2013-07-18 20:04:16 +0300

    Moved commonly used UpdateRefArgs method to newly created RefArgsExtensions extension class.

---
+++ src/VS 2010/KingAOP/Core/Methods/RefArgsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using KingAOP.Aspects;
+
+namespace KingAOP.Core.Methods
+{
+    internal static class RefArgsExtensions
+    {
+        public static Expression UpdateRefArgs(this Expression method, IEnumerable<DynamicMetaObject> originalArgs,  Expression aspectArgsEx)
+        {
+            var finallyBlock = originalArgs.Select((arg, index) =>
+                Expression.Assign(arg.Expression,
+                Expression.Convert(Expression.MakeIndex(
+                Expression.Property(aspectArgsEx, typeof(MethodInterceptionArgs).GetProperty("Arguments")),
+                typeof(Arguments).GetProperty("Item"), new[] { Expression.Constant(index) }), arg.RuntimeType))).ToList();
+
+            return Expression.TryFinally(method, Expression.Block(finallyBlock));
+        }
+    }
+}
