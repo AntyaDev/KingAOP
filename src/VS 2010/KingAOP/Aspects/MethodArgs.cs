14a74dc2ff523eaf803b8c5ab63c610b0a9677d2
Antya Dev <antyadev@gmail.com>
2013-07-22 01:08:03 +0300

    Added addition abstraction to method arguments - MethodArgs class.

---
+++ src/VS 2010/KingAOP/Aspects/MethodArgs.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace KingAOP.Aspects
+{
+    public abstract class MethodArgs : AdviceArgs
+    {
+        protected MethodArgs(object instance, MethodInfo method, Arguments arguments) : base(instance)
+        {
+            Method = method;
+            Arguments = arguments;
+        }
+
+        /// <summary>
+        /// Gets the method being executed.
+        /// </summary>
+        public MethodInfo Method { get; private set; }
+
+        /// <summary>
+        /// Gets the arguments with which the method has been invoked.
+        /// </summary>
+        public Arguments Arguments { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the method return value.
+        /// </summary>
+        public object ReturnValue { get; set; }
+    }
+}
