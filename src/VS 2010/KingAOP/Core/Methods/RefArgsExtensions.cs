1a183ff040bc235c2e4675021562aee8839c1a96
Antya Dev <antyadev@gmail.com>
2013-07-22 01:10:56 +0300

    Implemented UpdateArgumentsByRefParams method for RefArgsExtensions.

--- src/VS 2010/KingAOP/Core/Methods/RefArgsExtensions.cs
+++ src/VS 2010/KingAOP/Core/Methods/RefArgsExtensions.cs
@@ -8,13 +8,24 @@ namespace KingAOP.Core.Methods
 {
     internal static class RefArgsExtensions
     {
-        public static Expression UpdateRefArgs(this Expression method, IEnumerable<DynamicMetaObject> originalArgs,  Expression aspectArgsEx)
+        public static Expression UpdateRefParamsByArguments(this Expression method, IEnumerable<DynamicMetaObject> originalArgs, Expression aspectArgsEx)
         {
             var finallyBlock = originalArgs.Select((arg, index) =>
                 Expression.Assign(arg.Expression,
                 Expression.Convert(Expression.MakeIndex(
-                Expression.Property(aspectArgsEx, typeof(MethodInterceptionArgs).GetProperty("Arguments")),
+                Expression.Property(aspectArgsEx, typeof(MethodArgs).GetProperty("Arguments")),
                 typeof(Arguments).GetProperty("Item"), new[] { Expression.Constant(index) }), arg.RuntimeType))).ToList();
+
+            return Expression.TryFinally(method, Expression.Block(finallyBlock));
+        }
+
+        public static Expression UpdateArgumentsByRefParams(this Expression method, IEnumerable<DynamicMetaObject> originalArgs, Expression aspectArgsEx)
+        {
+            var finallyBlock = originalArgs.Select((arg, index) =>
+                Expression.Assign(Expression.MakeIndex(Expression.Property(aspectArgsEx,
+                typeof (MethodArgs).GetProperty("Arguments")),
+                typeof (Arguments).GetProperty("Item"), new[] {Expression.Constant(index)}),
+                Expression.Convert(arg.Expression, typeof(object)))).ToList();
 
             return Expression.TryFinally(method, Expression.Block(finallyBlock));
         }
